94a93ccd578f65297b4b98058e7d471a5a1d5bb0
Mohammad Bintang Wicaksono <mbintangw@gmail.com>
2024-09-06 15:08:26 +0700

    core : add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Background' : '#fffffe',
+        'Headline' : '#094067',
+        'Paragraph' : '#5f6c7b',
+        'Button' : '#3da9fc',
+        'ButtonText' : '#fffffe',
+        'Stroke' : '#094067',
+        'Main' : '#fffffe',
+        'Highlight' : '#3da9fc',
+        'Secondary' : '#90b4ce',
+        'Tertiary' : '#ef4565',
+      },
+
+    },
   },
   plugins: [],
 }
